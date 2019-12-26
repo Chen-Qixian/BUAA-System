03a977c8bc7ae993ff9e0a98862678e6722b41f7
chenqixian <chenqixian@buaa.edu.cn>
2019-12-26 12:01:23 +0800

    add anchor positioning

--- FrontEnd/js/front.js
+++ FrontEnd/js/front.js
@@ -31,31 +31,6 @@ $(function () {
     });
 
     // ------------------------------------------------------- //
-    // Material Inputs
-    // ------------------------------------------------------ //
-
-    var materialInputs = $('input.input-material');
-
-    // activate labels for prefilled values
-    materialInputs.filter(function() { return $(this).val() !== ""; }).siblings('.label-material').addClass('active');
-
-    // move label on focus
-    materialInputs.on('focus', function () {
-        $(this).siblings('.label-material').addClass('active');
-    });
-
-    // remove/keep label on blur
-    materialInputs.on('blur', function () {
-        $(this).siblings('.label-material').removeClass('active');
-
-        if ($(this).val() !== '') {
-            $(this).siblings('.label-material').addClass('active');
-        } else {
-            $(this).siblings('.label-material').removeClass('active');
-        }
-    });
-
-    // ------------------------------------------------------- //
     // Footer 
     // ------------------------------------------------------ //   
 
@@ -84,19 +59,6 @@ $(function () {
         $(this).find('.dropdown-menu').first().stop(true, true).fadeOut(100).removeClass('active');
     });
 
-
-    // ------------------------------------------------------- //
-    // Search Popup
-    // ------------------------------------------------------ //
-    $('.search-open').on('click', function (e) {
-        e.preventDefault();
-        $('.search-panel').fadeIn(100);
-    })
-    $('.search-panel .close-btn').on('click', function () {
-        $('.search-panel').fadeOut(100);
-    });
-
-
     // ------------------------------------------------------- //
     // Sidebar Functionality
     // ------------------------------------------------------ //
@@ -117,38 +79,11 @@ $(function () {
             $(this).find('i').attr('class', 'fa fa-long-arrow-left');
         }
     });
-
-
-    // ------------------------------------------------------ //
-    // For demo purposes, can be deleted
-    // ------------------------------------------------------ //
-
-    if ($('#style-switch').length > 0) {
-        var stylesheet = $('link#theme-stylesheet');
-        $("<link id='new-stylesheet' rel='stylesheet'>").insertAfter(stylesheet);
-        var alternateColour = $('link#new-stylesheet');
-
-        if ($.cookie("theme_csspath")) {
-            alternateColour.attr("href", $.cookie("theme_csspath"));
-        }
-
-        $("#colour").change(function () {
-
-            if ($(this).val() !== '') {
-
-                var theme_csspath = 'css/style.' + $(this).val() + '.css';
-
-                alternateColour.attr("href", theme_csspath);
-
-                $.cookie("theme_csspath", theme_csspath, {
-                    expires: 365,
-                    path: document.URL.substr(0, document.URL.lastIndexOf('/'))
-                });
-
-            }
-
-            return false;
-        });
-    }
-
+    $('#sidebar a').on('click', function() {
+        setTimeout(() => {
+            let scrolltop = Math.max( $("html").scrollTop(), $("body").scrollTop() );
+            console.log(scrolltop);
+            $(window).scrollTop(scrolltop - 80);
+        } , 0)
+    })
 });
